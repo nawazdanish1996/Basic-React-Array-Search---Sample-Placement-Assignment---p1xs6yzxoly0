82fadea0fa118c6285e274320b1d6093a71b4825
Nawaz Danish <nawazdanish1996@gmail.com>
2022-10-08 13:06:58 +0530

    updated final

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,60 @@
-import React, { useEffect, useState } from 'react'
-
 import '../styles/App.css';
-
+import React, { useState } from "react";
 
 const App = () => {
-  const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
-  const [data, setData] = useState(searchArray);
+  const searchArray = [
+    "Newton",
+    "School",
+    "Newton School",
+    "React",
+    "Preact",
+    "Node",
+    "Mongoose",
+    "Angular",
+    "Vue",
+    "Apple",
+    "Microsoft",
+    "Netflix",
+    "Meta"
+  ];
+  const [data, setData] = useState("");
+  const [input, setInput] = useState(searchArray);
 
-  const HandlerInput = (e) =>{
-    setData(e.target.value)
-  }
+  const HandlerInput = (e) => {
+    setData(e.target.value);
+    setInput(input);
+  };
 
-  useEffect(()=>{
-    HandlerInput();
-  },[])
-  
   return (
     <div id="main">
       <p>Search</p>
-      <input onChange={HandlerInput} value={data} id="search-input" type="text" />
+      <input
+        onChange={HandlerInput}
+        value={data}
+        id="search-input"
+        type="text"
+      />
       <p>Result</p>
 
-      {
-
-      }
+      {input
+        .filter((val) => {
+          if (data === "") {
+            return val;
+          } else if (val.toLowerCase().includes(data.toLowerCase())) {
+            return val;
+          }
+        })
+        .map((val, ind) => {
+          return (
+            <div key={ind}>
+              <ul>
+                <li>{val}</li>
+              </ul>
+            </div>
+          );
+        })}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
